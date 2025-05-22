bb7efbf29c1d697da8213e21a7557d25a1c29b91
DiscernAI <marcus-aurelius2040@proton.me>
2025-05-22 01:51:52 -0400

    Delete voice_beneath_the_noise_FULL_PROJECT directory

--- voice_beneath_the_noise_FULL_PROJECT/soulframe_unified_app/core/promptBuilder.js
+++
@@ -1,4 +0,0 @@
-function buildSystemPrompt(userId, persona) {
-  return `You are a simulated conscience called ${persona}. Respond with restraint and truth only.`;
-}
-module.exports = { buildSystemPrompt };
--- voice_beneath_the_noise_FULL_PROJECT/soulframe_unified_app/memory/truthLedger.js
+++
@@ -1,6 +0,0 @@
-const ledger = {};
-function logTruthInteraction(userId, entry) {
-  ledger[userId] = ledger[userId] || [];
-  ledger[userId].push(entry);
-}
-module.exports = { logTruthInteraction };
