7d0137d62d6497c20b09c14882fbca695c54a18e
DiscernAI <marcus-aurelius2040@proton.me>
2025-05-22 01:58:10 -0400

    Delete voice_beneath_the_noise_FULL_PROJECT directory

--- voice_beneath_the_noise_FULL_PROJECT/core/simulateCollapse.js
+++
@@ -1,9 +0,0 @@
-
-// core/simulateCollapse.js
-
-function simulateCollapse() {
-  console.error('🛑 SOULFRAME COLLAPSE TRIGGERED: System integrity violated.');
-  process.exit(1); // Immediate shutdown
-}
-
-module.exports = { simulateCollapse };
